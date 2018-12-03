83989102cf19bcc709c697b7e6178607ebc73f76
cyril4591 <cyril4591@gmail.com>
2018-12-03 15:48:06 +0100

    Class creation Matrix and DataSet

---
+++ OCR/DataSet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OCR
+{
+    public class DataSet
+    {
+        public char[] Char;
+        public Matrix[] TabMatrix;
+
+        public DataSet(char[] Char, Matrix[] TabMatrix)
+        {
+            Char = this.Char;
+            TabMatrix = this.TabMatrix;
+        }
+    }
+}
---
+++ OCR/Matrix.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OCR
+{
+    public class Matrix
+    {
+        public int[,] matrix;
+
+        Matrix(int[,] matrix)
+        {
+            matrix = this.matrix;
+        }
+    }
+}
