3e6b9b10bdcad7dd0dacbb873660ad2971eff241
florian-marc <florian.marc@devinci.fr>
2018-12-04 21:47:16 +0100

    added a script that produce a visual interpretation of the data

--- OCR/data_vizualization.py
+++ OCR/data_vizualization.py
@@ -1,9 +1,9 @@
 import sys
-print(sys.path)
 import os
-
-#import numpy as np
-#import matplotlib.pyplot as plt
+dirname = os.path.dirname
+print(os.path.join( dirname(dirname(dirname(__file__))), 'py', 'py' + str(1) +'.png'))
+import numpy as np
+import matplotlib.pyplot as plt
 
 class DataMatrix:
     n_metadata = 6
@@ -12,17 +12,17 @@ class DataMatrix:
     n_rows = 16
 
     def __init__(self, input):
-        #self.data = np.zeros((self.n_rows, self.n_cols))
+        self.data = np.zeros((self.n_rows, self.n_cols))
 
         #data preprocessing
-        formatted_input = "".join(input.split('\t'))
+        #formatted_input = "".join(input.split('\t'))
+        formatted_input = input.split('\t')
         self.letter = formatted_input[self.letter_position]
         #Using a row of the input file as an input, we'll put the data in a
         #numpy matrix
         for y in range(0, self.n_rows):
             for x in range(0, self.n_cols):
-                print()
-                #data[y, x] = formatted_input[self.n_metadata + (self.n_cols * y) + x]
+                self.data[y, x] = formatted_input[self.n_metadata + (self.n_cols * y) + x]
 
 
 
@@ -32,26 +32,21 @@ class DataSet:
         self.dataFrame = []
         #Opening data file
         f = open(input_file, 'r')
-        n_lines = count = len(f.readlines(  ))
-        for i in range(0, 2):
+        n_lines = len(f.readlines())
+        
+        #Returning at the beginning of the file
+        f.seek(0)
+        for i in range(n_lines):
             self.dataFrame.append(DataMatrix(f.readline()))
-
-        f.close();
+            plt.imshow(self.dataFrame[i].data)
+            plt.savefig(os.path.join( dirname(dirname(dirname(__file__))), 'py', 'py' + str(i) +'.png'))
+        f.close()
 
 filename = 'letter.data'
 path = 'OCR/data'
-f=open(os.path.join(path, filename), 'r')
-str = f.readline()
-#str
-formatted_input = "".join(str.split('\t'))
-len(formatted_input)
-letter = formatted_input[1]
-letter
-#formatted_input
-f.close()
-
-#os.listdir()
-
 
 dataset = DataSet(os.path.join(path, filename))
-dataset.dataFrame[0]
+#plt.plot(dataset.dataFrame[0])
+#for i in range(0, 9):
+ #   plt.imshow(dataset.dataFrame[i].data)
+  #  plt.savefig(os.path.join( dirname(dirname(dirname(__file__))), 'py', 'py' + str(i) +'.png'))
