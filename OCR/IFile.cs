68893a45fba8fc53114e0119df284fecb3565d11
florian-marc <florian.marc@devinci.fr>
2018-12-05 20:04:03 +0100

    Added interface IFile

---
+++ OCR/IFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OCR
+{
+    public interface IFile
+    {
+        void OpenFile();
+
+        void OpenFile(string path);
+
+        void CloseFile();
+
+        List<string> GetLine();
+    }
+}
