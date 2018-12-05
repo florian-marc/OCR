8614a722b19e76595fb49f687128f29286fb3295
florian-marc <florian.marc@devinci.fr>
2018-12-05 20:08:12 +0100

    changed OpenFile class name to FileRead to avoid confusion with methods names

---
+++ OCR/FileRead.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace OCR
+{
+    public class FileRead:IFile
+    {
+        string path;
+        FileStream file;
+
+        public FileRead(string path)
+        {
+            this.path = path;
+            file = null;
+        }
+
+        public void OpenFile()
+        {
+            file = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.None);
+        }
+
+        public void OpenFile(string path)
+        {
+
+        }
+
+        public void CloseFile()
+        {
+            file.Close();
+        }
+
+        private string _GetLine()
+        {
+
+        }
+
+        public List<string> GetLine()
+        {
+
+        }
+
+        public string Path
+        {
+            get { return this.path; }
+        }
+    }
+}
