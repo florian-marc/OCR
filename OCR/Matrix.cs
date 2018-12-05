6d1783763b7e89cf97af9a49b3b4005b6a867c68
cyril4591 <cyril4591@gmail.com>
2018-12-05 18:39:21 +0100

    constructor for matrix class

--- OCR/Matrix.cs
+++ OCR/Matrix.cs
@@ -7,8 +7,25 @@ namespace OCR
 {
     public class Matrix
     {
+        private static int ligne = 16;
+        private static int colone = 8;
 
-        public static int[,] matrix = new int[16, 8];
+        private int[,] matrix = new int[ligne, colone];
+
+        public Matrix(List<int> listbin)
+        {
+            int ligne = 0;
+
+            while(ligne!=15)
+            {
+                int colone = 0;
+                while (colone!=8)
+                {
+                    this.matrix[ligne, colone] = listbin[ligne * 8 + colone];
+                }
+                ligne++;
+            }
+        }
 
     }
 }
