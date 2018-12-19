f1e18bacb52a323d6132d0ab1c70bae931212f2a
florian-marc <florian.marc@devinci.fr>
2018-12-19 10:18:36 +0100

    Changed path

--- OCR/Program.cs
+++ OCR/Program.cs
@@ -11,8 +11,9 @@ namespace OCR
         //nombre of elements in the data we have: 52152
         static void Main(string[] args)
         {
+            string sourcePath = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).FullName).FullName;
             string[] paths = {"data", "letter.data"};
-            FileRead file = new FileRead(Path.Combine(paths[0], paths[1]));
+            FileRead file = new FileRead(Path.Combine(sourcePath, Path.Combine(paths[0], paths[1])));
             DataSet Data = new DataSet(file.GetNumberOfLines);
 
             for(int i = 0; i<52152; i++)
